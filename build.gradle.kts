5a1981afa062ad98aa17d3ee0d1289119649a336
vvb2060 <vvb2060@gmail.com>
2023-09-04 01:21:19 +0800

    Prepare to publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
 
 plugins {
+    id("signing")
     id("maven-publish")
     id("com.android.library") version "8.1.1" apply false
 }
@@ -32,6 +33,7 @@ subprojects {
 
         if (plugins.hasPlugin("com.android.library")) {
             apply(plugin = "maven-publish")
+            apply(plugin = "signing")
 
             androidLibrary {
                 buildFeatures {
@@ -55,7 +57,47 @@ subprojects {
                         groupId = "com.github.topjohnwu.libsu"
                         version = "5.2.0"
                         artifactId = project.name
+                        pom {
+                            name.set("libsu")
+                            description.set("An Android library providing a complete solution for apps using root permissions")
+                            url.set("https://github.com/topjohnwu/libsu")
+                            licenses {
+                                license {
+                                    name.set("Apache License 2.0")
+                                    url.set("https://github.com/topjohnwu/libsu/blob/master/LICENSE")
+                                }
+                            }
+                            developers {
+                                developer {
+                                    name.set("John Wu")
+                                    url.set("https://github.com/topjohnwu")
+                                }
+                            }
+                            scm {
+                                connection.set("scm:git:https://github.com/topjohnwu/libsu.git")
+                                url.set("https://github.com/topjohnwu/libsu")
+                            }
+                        }
                     }
+                }
+                repositories {
+                    maven {
+                        name = "ossrh"
+                        url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                        credentials(PasswordCredentials::class)
+                    }
+                }
+            }
+
+            signing {
+                val signingKey = findProperty("signingKey") as String?
+                val signingPassword = findProperty("signingPassword") as String?
+                val secretKeyRingFile = findProperty("signing.secretKeyRingFile") as String?
+                if (secretKeyRingFile != null && file(secretKeyRingFile).exists()) {
+                    sign(publishing.publications)
+                } else if (signingKey != null && signingPassword != null) {
+                    useInMemoryPgpKeys(signingKey, signingPassword)
+                    sign(publishing.publications)
                 }
             }
         }
